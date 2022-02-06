022c1853fff19ad5e5322f72d49be9a8d4cfc680
baiest <ballesteros.juan@correounivalle.edu.co>
2022-02-06 16:26:29 -0500

    Add menu, products list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { MenuComponent } from './components/menu/menu.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductsListComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/products-list/products-list.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.scss'],
+})
+export class ProductsListComponent implements OnInit {
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Producto 1',
+      image: '../assets/images/album.jpg',
+      price: 30000,
+    },
+    {
+      id: '2',
+      name: 'Bike',
+      image: '../assets/images/bike.jpg',
+      price: 25000,
+    },
+    {
+      id: '3',
+      name: 'Books',
+      image: '../assets/images/books.jpg',
+      price: 65000,
+    },
+    {
+      id: '4',
+      name: 'Glasses',
+      image: '../assets/images/glasses.jpg',
+      price: 49000,
+    },
+    {
+      id: '5',
+      name: 'House',
+      image: '../assets/images/house.jpg',
+      price: 20000,
+    },
+    {
+      id: '6',
+      name: 'Toy',
+      image: '../assets/images/toy.jpg',
+      price: 15000,
+    },
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
